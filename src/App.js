f084209ed2dd2db0b01e884b2e35cd1b4c175a35
neethimanda09 <neethimanda09@gmail.com>
2023-12-04 11:02:41 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
 import Board from './WhiteBoard/Board';
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 
 
-function App(props) {
+function App() {
   
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import WhiteBoard from './WhiteBoard/Board';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
